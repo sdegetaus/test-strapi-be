f9c8c1cdcb768e7d38c0c5e2a87e89baa59eb954
Santiago Degetau Sada <santiago.degetau@gmail.com>
2021-03-12 11:11:53 -0600

    Strapi Quickstart

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e7f5e0416c7feb08f525572766546bee'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e5ab152b-0662-41ea-b269-d31afdb1e5f1'
+};
